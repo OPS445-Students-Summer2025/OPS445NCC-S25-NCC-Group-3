4e7415c95fea516be3b5d589bf22a675fd6d91ac
mzaman30 <shahriarzaman755@gmail.com>
2025-08-05 23:47:50 -0400

    Delete Login_Activity_Analyzer.py

--- Login_Activity_Analyzer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# Student ID: mzaman30
-
-from assignment2 import login_activity_analyzer
-
-if __name__ == "__main__":
-    print("=== Testing Login Activity Analyzer ===\n")
-    login_activity_analyzer()
